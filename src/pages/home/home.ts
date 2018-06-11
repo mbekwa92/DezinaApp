66a77ae1042b9d377ce03f10ab9eec7742a16342
Ntsikelelo <mbekwa92@gmail.com>
2018-06-11 11:01:29 +0200

    Initial Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {Http} from "@angular/http";
+import "rxjs/add/operator/map"
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(private http:Http, public navCtrl: NavController) {
+
+  }
+
+
+  ionViewDidLoad(){
+    this.http.get("/assets/data.json")
+    .map(response=>response.json())
+    .subscribe(data=> console.log(data));
+  }
+
+}
