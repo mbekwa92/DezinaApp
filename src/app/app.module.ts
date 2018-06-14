469eb6bad125f8698e334baad9e0c348a8db4e6a
Ntsikelelo <mbekwa92@gmail.com>
2018-06-14 16:38:50 +0200

    Register new Filter Modal page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import {FilterModalPage} from "../pages/filter-modal/filter-modal";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -21,7 +22,8 @@ import {ProductDetailPage} from '../pages/product-detail/product-detail';
     ContactPage,
     HomePage,
     TabsPage,
-    ProductDetailPage
+    ProductDetailPage,
+    FilterModalPage
   ],
 
   imports: [
@@ -37,7 +39,8 @@ import {ProductDetailPage} from '../pages/product-detail/product-detail';
     ContactPage,
     HomePage,
     TabsPage,
-    ProductDetailPage
+    ProductDetailPage,
+    FilterModalPage
   ],
   providers: [
     StatusBar,
