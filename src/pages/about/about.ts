c56ea6fb265dd91b036c2375c893dcb4b5394694
Ntsikelelo <mbekwa92@gmail.com>
2018-06-14 16:07:23 +0200

    Display only bestSellers and display in About page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {ProductProvider} from "../../providers/product/product";
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  public bestSellerProducts = [];
+  constructor(private productService:ProductProvider, public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController) {
+  }
+
+  ionViewDidLoad(){
+    this.productService.getProducts()
+    .subscribe((allProducts)=>{
+      this.bestSellerProducts = allProducts.filter(product=>product.bestSeller ==true);
+      console.log(this.bestSellerProducts);
+    });
 
   }
 
